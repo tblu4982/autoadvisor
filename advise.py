409a1d92fab18dd02e4bd43bf3b00096a27cdba1
Terrence <121534028+tblu4982@users.noreply.github.com>
2023-02-01 16:46:42 -0500

    Generates advisory report
    Found a more concise method to check all courses for passing grades. Rewrote advising logic; it now checks to see if a student has passed a class regardless of if they're missing prequisites.

--- advise.py
+++ advise.py
@@ -2,7 +2,68 @@ from preprocess import courses
 import re
 from datetime import datetime
 
-#-------create a global variable for core credits and total credits-----
+#90 total credits and 72 core class credits before csci 400 and past csci 287
+#students become eligible for 400 level courses if they have 75+ credits
+
+#method to check if a course has a passing grade where a passing grade is 'C'
+def passing_grade(grade, sem, curr_sems):
+    if grade <= "C" or grade == "S" or grade == "TR" or grade == "In progress" \
+       or grade == "SP":
+        #add a check for future courses, which should be omitted
+        #from course recommendations
+        #If course is from this semester, include it
+        if grade == "In progress":
+            if sem == curr_sems:
+                return True
+            #If course is from future semester, omit it
+            else:
+                return False
+        #If course is not from this semester, then it has already
+        #been taken
+        else:
+            return True
+    #If we reached this, then we reached a failing course somehow
+    else:
+        return False
+
+#method to check if a course has a passing grade where a passing grade is 'D'
+def passing_grade_alt(grade, sem, curr_sems):
+    if grade <= "D" or grade == "S" or grade == "TR" or grade == "In progress" \
+       or grade == "SP":
+        if grade == "In progress":
+            if sem == curr_sems:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def csci_free_elec(course):
+    pass
+
+def gs_elec(course):
+    pass
+
+def ss_elec(course):
+    pass
+
+def cms_elec(course):
+    pass
+
+def lit_elec(course):
+    pass
+
+def hist_elec(course):
+    pass
+
+def bcp_elec(course):
+    pass
+
+def math_elec(course):
+    pass
+    
 
 def get_curr_sem():
     month = datetime.now().month
@@ -20,40 +81,16 @@ def get_curr_sem():
         else:
             return "Spring " + str(year)
 
-#checks csci courses for passing grade in terms eligibility
-def csci_course_check (grade, sem, curr_sem):
-    if grade <= "C" or grade == "S" or grade == "TR" or grade == "In progress" \
-       or grade == "SP":
-        #add a check for future courses, which should be omitted
-        #from course recommendations
-        #If course is from this semester, include it
-        if grade == "In progress":
-            if sem == curr_sem:
-                return True
-            #If course is from future semester, omit it
-            else:
-                return False
-        #If course is not from this semester, then it has already
-        #been taken
-        else:
-            return True
-    #If we reached this, then we reached a failing course somehow
-    else:
-        return False
-
-#checks other courses for passing grade in terms of eligibility
-def course_check (grade, sem, curr_sem):
-    if grade <= "D" or grade == "S" or grade == "TR" or grade == "In progress" \
-       or grade == "SP":
-        if grade == "In progress":
-            if sem == curr_sem:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+#Dictionary to hold core course stack, as we traverse through course matrix,
+#we'll set dict values to true as we find them
+core_course_stack = {"MATH 260": False, "CSCI 101": False, "CSCI 150": False, "CSCI 151": False,
+                     "ENGL 110": False, "MATH 261": False, "CSCI 250": False, "CSCI 251": False,
+                     "ENGL 111": False, "MATH 280": False, "CSCI 287": False, "CSCI 303": False,
+                     "ENGL 342": False, "CSCI 281": False, "CSCI 356": False, "CSCI 296": False,
+                     "PHIL 450": False, "PHIL 275": False, "STAT 340": False, "CSCI 392": False,
+                     "CSCI 487": False, "CSCI 445": False, "CSCI 358": False, "CSCI 489": False,
+                     "CSCI 400": False, "CSCI 471": False, "CSCI 493": False, "CSCI 485": False,
+                     "CSCI 494": False}    
 
 #array to store the current/future semesters
 curr_sems = []
@@ -65,10 +102,13 @@ csci = 0
 arts = 0
 in_prog = 0
 pending = 0
-credit_hours = 0
+core_credits = 0
+total_credits = 0
 
 for c in courses:
-    credit_hours += float(c[3])
+    total_credits += float(c[3])
+    if c[0] in core_course_stack:
+        core_credits += float(c[3])
     if re.search("ARTS", c[0]):
         arts += 1
     elif re.search("CSCI", c[0]):
@@ -93,141 +133,35 @@ print("ARTS Classes: ", str(arts))
 print("CSCI Classes: ", str(csci))
 print("Classes In Progress: ", str(in_prog))
 print("Future Classes: ", str(pending))
+print("Core Credits: ", str(core_credits))
+print("Total Credits: ", str(total_credits))
 print("------------------------------")
 
 #check course stacks
-#course, name, grade, credits, semester
-#boolean values that hold whether or not a course was passed
-m120 = m121 = m260 = m261 = m280 = s340 = False
-cs101 = cs150 = cs151 = cs250 = cs251 = False
-cs281 = cs287 = cs296 = cs303 = cs356 = False
-cs358 = cs392 = cs400 = cs445 = cs471 = False
-cs485 = cs487 = cs489 = cs493 = cs494 = False
-e110 = e111 = e342 = phil = False
-phil_n = ""
 
 #loop through matrix to find courses matching requirements
 for c in courses:
-    if c[0] == "MATH 260" or m260 and not s340:
-        if course_check (c[2], c[4], curr_sems[0]):
-            m260 = True
-            if c[0] == "MATH 261" or m261:
-                if course_check (c[2], c[4], curr_sems[0]):
-                    m261 = True
-                    if c[0] == "STAT 340" and cs281:
-                        if course_check (c[2], c[4], curr_sems[0]):
-                            s340 = True
-
-    if c[0] == "MATH 120" and not m120 and not m260:
-        if course_check (c[2], c[4], curr_sems[0]):
-            m120 = True
+    passed_course = False
+    if c[0] in core_course_stack:
+        if c[0] == "PHIL 450" or c[0] == "PHIL 275" or c[0] == "ENGL 342":
+            passed_course = passing_grade_alt(c[2], c[4], curr_sems)
+        else:
+            passed_course = passing_grade(c[2], c[4], curr_sems)
+        if passed_course:
+            core_course_stack[c[0]] = True
             
-    if c[0] == "MATH 121" and not m280 and not m260:
-        if course_check (c[2], c[4], curr_sems[0]):
-            m121 = True
-            
-    if c[0] == "MATH 280" and m121 or m280 or not m121 and m260 and not cs392:
-        if course_check (c[2], c[4], curr_sems[0]):
-            m280 = True
-            if c[0] == "CSCI 281" or cs281:
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs281 = True
-                    if c[0] == "CSCI 392" and cs287:
-                        if csci_course_check (c[2], c[4], curr_sems[0]):
-                            cs392 = True
-
-    
-                            
-    if c[0] == "CSCI 101" and not cs101:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs101 = True
-
-    if c[0] == "CSCI 150" and not cs150:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs150 = True
-
-    if c[0] == "CSCI 151" and not cs151:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs151 = True
-
-    if c[0] == "CSCI 250" and cs150 and cs151 and not cs250:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs250 = True
-
-    if c[0] == "CSCI 251" and cs150 and cs151 and not cs251:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs251 = True
-
-    if c[0] == "CSCI 296" and cs250 and cs251 and not cs296:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs296 = True
-
-    if c[0] == "CSCI 356" and cs250 and cs251 and not cs356:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs356 = True
-
-    if c[0] == "CSCI 358" and cs250 and cs251 and not cs358:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs358 = True
-
-    if c[0] == "CSCI 287" or cs287 and cs250 and cs251:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs287 = True
-            if c[0] == "CSCI 487":
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs487 = True
-            if c[0] == "CSCI 471":
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs471 = True
-            if c[0] == "CSCI 485":
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs485 = True
-
-    if c[0] == "CSCI 303" or cs303 and cs250 and 251:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs303 = True
-            if c[0] == "CSCI 489":
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs489 = True
-            if c[0] == "CSCI 445":
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs445 = True
-
-    if c[0] == "CSCI 400" or cs400 and credit_hours > 75 and not cs494:
-        if csci_course_check (c[2], c[4], curr_sems[0]):
-            cs400 = True
-            if c[0] == "CSCI 493" or cs493:
-                if csci_course_check (c[2], c[4], curr_sems[0]):
-                    cs493 = True
-                    if c[0] == "CSCI 494":
-                        cs494 = True
-
-    if c[0] == "ENGL 110" or e110 and not e342:
-        if course_check (c[2], c[4], curr_sems[0]):
-            e110 = True
-            if c[0] == "ENGL 111" or e111:
-                if course_check (c[2], c[4], curr_sems[0]):
-                    e111 = True
-                    if c[0] == "ENGL 342":
-                        if course_check (c[2], c[4], curr_sems[0]):
-                            e342 = True
-
-    if c[0] == "PHIL 450" or c[0] == "PHIL 275" and not phil:
-        if course_check (c[2], c[4], curr_sems[0]):
-            phil_n = c[0]
-            phil = True
 #list to hold course recommendations
 recommendations = []
 
 #Math courses stack
 print("MATH:", end= " ")
-if m260:
+if core_course_stack["MATH 260"]:
     print("MATH 260", end = " ")
-    if m261:
+    if core_course_stack["MATH 261"]:
         print(", MATH 261", end = " ")
-        if s340 and cs281:
+        if core_course_stack["STAT 340"] and core_course_stack["CSCI 281"]:
             print(", STAT 340", end = " ")
-        elif cs281:
+        elif core_course_stack["CSCI 281"]:
             recommendations.append("Recommend STAT 340")
         else:
             print("\nNeed CSCI 281 before STAT 340")
@@ -236,108 +170,108 @@ if m260:
 else:
     recommendations.append("Recommend MATH 260")
 
-if m280:
+if core_course_stack["MATH 280"]:
     print("MATH 280", end = " ")
 
 print("\n------------------------------")
 
 #CSCI courses stack
 print("CSCI:", end= " ")
-if cs101:
+if core_course_stack["CSCI 101"]:
     print("CSCI 101", end= " ")
 else:
     recommendations.append("Recommend CSCI 101")
 
-if cs150:
+if core_course_stack["CSCI 150"]:
     print("CSCI 150", end= " ")
 else:
     recommendations.append("Recommend CSCI 150")
 
-if cs151:
+if core_course_stack["CSCI 151"]:
     print("CSCI 151", end= " ")
 else:
     recommendations.append("Recommend CSCI 151")
 
-if m280:
-    if cs281:
+if core_course_stack["MATH 280"]:
+    if core_course_stack["CSCI 281"]:
         print("CSCI 281", end= " ")
     else:
         recommendations.append("Recommend CSCI 281")
-        if cs392 and cs287:
+        if core_course_stack["CSCI 392"] and core_course_stack["CSCI 287"]:
             print("CSCI 392", end= " ")
-        elif cs287:
+        elif core_course_stack["CSCI 287"]:
             recommendations.append("Recommend CSCI 392")
         else:
             print("\nNeed CSCI 287 before CSCI 392")
 
-if cs150 and cs151:
-    if cs250:
+if core_course_stack["CSCI 150"] and core_course_stack["CSCI 151"]:
+    if core_course_stack["CSCI 250"]:
         print("CSCI 250", end= " ")
     else:
         recommendations.append("Recommend CSCI 250")
 
-    if cs251:
+    if core_course_stack["CSCI 251"]:
         print("CSCI 251", end= " ")
     else:
         recommendations.append("Recommend CSCI 251")
 
-    if cs250 and cs251:
-        if cs296:
+    if core_course_stack["CSCI 250"] and core_course_stack["CSCI 251"]:
+        if core_course_stack["CSCI 296"]:
             print("CSCI 296", end= " ")
         else:
             recommendations.append("Recommend CSCI 296")
 
-        if cs356:
+        if core_course_stack["CSCI 356"]:
             print("CSCI 356", end= " ")
         else:
             recommendations.append("Recommend CSCI 356")
 
-        if cs358:
+        if core_course_stack["CSCI 358"]:
             print("CSCI 358", end= " ")
         else:
             recommendations.append("Recommend CSCI 358")
 
-        if cs287:
+        if core_course_stack["CSCI 287"]:
             print("CSCI 287", end= " ")
-            if cs487:
+            if core_course_stack["CSCI 487"]:
                 print("CSCI 487", end= " ")
             else:
                 recommendations.append("Recommend CSCI 487")
-            if cs471:
+            if core_course_stack["CSCI 471"]:
                 print("CSCI 471", end= " ")
             else:
                 recommendations.append("Recommend CSCI 471")
-            if cs485:
+            if core_course_stack["CSCI 485"]:
                 print("CSCI 485", end= " ")
             else:
                 recommendations.append("Recommend CSCI 485")
         else:
             recommendations.append("Recommend CSCI 287")
 
-        if cs303:
+        if core_course_stack["CSCI 303"]:
             print("CSCI 303", end= " ")
-            if cs489:
+            if core_course_stack["CSCI 489"]:
                 print("CSCI 489", end= " ")
             else:
                 recommendations.append("Recommend CSCI 489")
-            if cs445:
+            if core_course_stack["CSCI 445"]:
                 print("CSCI 445", end= " ")
             else:
                 recommendations.append("Recommend CSCI 445")
         else:
             recommendations.append("Recommend CSCI 303")
 
-if cs400:
+if core_course_stack["CSCI 400"]:
     print("CSCI 400", end= " ")
-    if cs493:
+    if core_course_stack["CSCI 493"]:
         print("CSCI 493", end= " ")
-        if cs494:
+        if core_course_stack["CSCI 494"]:
             print("CSCI 494", end= " ")
         else:
             recommendations.append("Recommend CSCI 494")
     else:
         recommendations.append("Recommend CSCI 493")
-elif credit_hours > 75:
+elif total_credits > 75:
     recommendations.append("Recommend CSCI 400")
 
 print("\n------------------------------")
@@ -345,11 +279,11 @@ print("\n------------------------------")
 #English courses stack
 print("ENGL:", end= " ")
 
-if e110:
+if core_course_stack["ENGL 110"]:
     print("ENGL 110", end= " ")
-    if e111:
+    if core_course_stack["ENGL 111"]:
         print("ENGL 111", end= " ")
-        if e342:
+        if core_course_stack["ENGL 342"]:
             print("ENGL 342", end= " ")
         else:
             recommendations.append("Recommend ENGL 342")
@@ -363,8 +297,11 @@ print("\n------------------------------")
 #Miscellaneous courses stack
 print("MISC:", end = " ")
 
-if phil:
-    print(phil_n, end= " ")
+if core_course_stack["PHIL 450"] or core_course_stack["PHIL 275"]:
+    if core_course_stack["PHIL 450"]:
+        print("PHIL 450", end= " ")
+    else:
+        print("PHIL 275", end= " ")
 else:
     recommendations.append("Recommend PHIL 450 or PHIL 275")
 
@@ -372,3 +309,4 @@ else:
 print('\n------------------------------')
 for n in recommendations:
     print(n)
+
